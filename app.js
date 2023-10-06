f462c4519ea857d947863a3e8303f66af15abba4
TrevorMickelson <social@trevmick.com>
2023-10-06 09:03:14 -0500

    update

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
-const apiRouter = require('./app_server/routes/index');
+const apiRouter = require('./app_api/routes/index');
 
 const hbs = require('hbs');
 
